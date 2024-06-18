2b73444e1f8b90dcee59f521752e65b3658abb4a
huy.ltn204986 <huy.ltn204986@sis.hust.edu.vn>
2024-06-18 17:45:53 +0700

    thiet ke giao dien thanh broad bar

--- src/pages/Boards/BroadBar/index.jsx
+++ src/pages/Boards/BroadBar/index.jsx
@@ -1,15 +1,126 @@
 import Box from '@mui/material/Box'
+import Chip from '@mui/material/Chip'
+import Avatar from '@mui/material/Avatar'
+import AvatarGroup from '@mui/material/AvatarGroup'
+import Tooltip from '@mui/material/Tooltip'
+import Button from '@mui/material/Button'
+
+import DashboardIcon from '@mui/icons-material/Dashboard'
+import VpnLockIcon from '@mui/icons-material/VpnLock'
+import AddToDriveIcon from '@mui/icons-material/AddToDrive'
+import BoltIcon from '@mui/icons-material/Bolt'
+import FilterListIcon from '@mui/icons-material/FilterList'
+import PersonAddIcon from '@mui/icons-material/PersonAdd'
+
+const MENU_STYLES = {
+  color: 'primary.main',
+  bgcolor: 'white',
+  border: 'none',
+  paddingX: '5px',
+  borderRadius: '4px',
+  '& .MuiSvgIcon-root': {
+    color: 'primary.main'
+  },
+  '&:hover': {
+    bgcolor: 'primary.50'
+  }
+}
 
 const BroadBar = () => {
   return (
     <Box sx={{
-      backgroundColor: 'primary.dark',
+      // backgroundColor: 'primary.dark',
       width: '100%',
       height: (theme) => theme.trello.broadBarHeight,
       display: 'flex',
-      alignItems: 'center'
+      alignItems: 'center',
+      justifyContent: 'space-between',
+      gap: 2,
+      paddingX: 2,
+      overflowX: 'auto',
+      borderTop: '1px solid #00bfa5'
     }}>
-      Broad bar
+      <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+        <Chip
+          sx={MENU_STYLES}
+          icon={<DashboardIcon />}
+          label="Trello Board"
+          clickable
+        />
+
+        <Chip
+          sx={MENU_STYLES}
+          icon={<VpnLockIcon />}
+          label="Public/Private Workspace"
+          clickable
+        />
+
+        <Chip
+          sx={MENU_STYLES}
+          icon={<AddToDriveIcon />}
+          label="Add to Google Drive"
+          clickable
+        />
+
+        <Chip
+          sx={MENU_STYLES}
+          icon={<BoltIcon />}
+          label="Automation"
+          clickable
+        />
+
+        <Chip
+          sx={MENU_STYLES}
+          icon={<FilterListIcon />}
+          label="Filters"
+          clickable
+        />
+
+      </Box>
+      <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+        <Button variant="outlined" startIcon={<PersonAddIcon />}>Invite</Button>
+        <AvatarGroup
+          max={4}
+          sx={{
+            '& .MuiAvatar-root': {
+              width: 34,
+              height: 34,
+              fontSize: 16
+            }
+          }}
+        >
+          <Tooltip title="avatar">
+            <Avatar
+              alt="Rem Sharp"
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRa9UI1y4Shr253ibETjPCOWNBWWpKgQsVKaw&s" />
+          </Tooltip>
+          <Tooltip title="avatar">
+            <Avatar
+              alt="Rem Sharp"
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6vjzwZaBf6bKmbl7I00WbZ9RPOlriawksgQ&s" />
+          </Tooltip>
+          <Tooltip title="avatar">
+            <Avatar
+              alt="Rem Sharp"
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfqR64K85wa8CquD0SrMbQaFNqgiTNN2ftiw&s" />
+          </Tooltip>
+          <Tooltip title="avatar">
+            <Avatar
+              alt="Rem Sharp"
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRa9UI1y4Shr253ibETjPCOWNBWWpKgQsVKaw&s" />
+          </Tooltip>
+          <Tooltip title="avatar">
+            <Avatar
+              alt="Rem Sharp"
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRa9UI1y4Shr253ibETjPCOWNBWWpKgQsVKaw&s" />
+          </Tooltip>
+          <Tooltip title="avatar">
+            <Avatar
+              alt="Rem Sharp"
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRa9UI1y4Shr253ibETjPCOWNBWWpKgQsVKaw&s" />
+          </Tooltip>
+        </AvatarGroup>
+      </Box>
     </Box>
   )
 }
